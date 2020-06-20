d43e238b65796d8a0a13c121976a7cc94210c454
Isaac <iboates@gmail.com>
2020-06-20 20:43:49 +0200

    streamlining cloud deployment

---
+++ sh/00_download_anaconda.sh
@@ -0,0 +1,2 @@
+wget https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh -o anaconda_insaller.sh
+chmod +x anaconda_insaller.sh
---
+++ sh/01_create_conda_env.sh
@@ -0,0 +1,3 @@
+conda create --name stylegan2 --python=3.6
+conda activate stylegan2
+pip install -r ../requirements.txt
